2177514e27904f67bf4ebf593e297c949bff142e
Mohamad Masalmeh <40610458+mohamad-msalme@users.noreply.github.com>
2022-03-07 01:09:13 +0300

    Refactor Guest list to be more resuble

--- rts/src/partyguestlist/Container.tsx
+++ rts/src/partyguestlist/Container.tsx
@@ -2,24 +2,35 @@ import React, { useState } from 'react';
 
 interface ContainerProps {
   title?: string;
+  buttonLabel?: string;
+  inputLabel?: string;
 }
-const Container: React.FunctionComponent<ContainerProps> = ( { title } ) => {
+const Container: React.FunctionComponent<ContainerProps> = ( { title, buttonLabel, inputLabel } ) => {
   const [guests, addGuest] = useState<string[]>([]);
   const [text, setText] = useState<string>('');
   const renderListOfGuest = (): JSX.Element[] | null => guests.map((guest) => <li key={guest}>{guest}</li>);
+  const onClickAdd = (): void => {
+    if (text) {
+      addGuest([...guests, text]);
+      setText('');
+    }
+  }
 
   return (
   <React.Fragment>
-    <h3>Guest List</h3>
+    <h3>{title}</h3>
     <ul>{renderListOfGuest()}</ul>
-    <input type='text' value={text} onChange={(e) => setText(e.target.value)}></input>
-    <button type='submit' onClick={() => text && addGuest([...guests, text])}>{title}</button>
+    <label htmlFor="guest-input">{inputLabel}</label>
+    <input type='text' id="guest-input" value={text} onChange={(e) => setText(e.target.value)}></input>
+    <button type='submit' onClick={onClickAdd}>{buttonLabel}</button>
   </React.Fragment>
   ) 
 }
 
 Container.defaultProps = {
-  title: 'Add Guest',
+  title: 'Guest List',
+  buttonLabel: 'Add Guest',
+  inputLabel: 'Guest Name:'
 }
 
 export default Container;
